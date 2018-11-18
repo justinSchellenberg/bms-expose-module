81b9d2b8e28ffc5b276d1c2816003df0b501ac4b
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2018-11-18 17:26:59 -0600

    Update Package and include dev dependencies

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
+import GetObjectKeys from 'bms-utility-helper-functions/src/GetObjectKeys'
 
+G
 
 export default function ExposeModule(module: Function, namespace: object){
   // if object is empty, just fail and return now.
